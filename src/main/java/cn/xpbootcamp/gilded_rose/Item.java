4a4cd6d29f2e1564486cd000ebfc38a32c97b568
Qing Chen <qing.chen@thoughtworks.com>
2020-03-08 20:44:25 +0800

    simple clean if condition and extract some methods

--- src/main/java/cn/xpbootcamp/gilded_rose/Item.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Item.java
@@ -20,10 +20,20 @@ public class Item {
     }
 
     public void updateOneDay() {
-        if (!name.equals("Aged Brie")
-                && !name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        updateQuality();
+
+        updateSellIn();
+
+        if (isExpired()) {
+            updateQualityWhenExpiration();
+        }
+    }
+
+    public void updateQuality() {
+        if (!isAgedBrie()
+                && !isBackstage()) {
             if (quality > 0) {
-                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (!isSulfuras()) {
                     quality = quality - 1;
                 }
             }
@@ -31,7 +41,7 @@ public class Item {
             if (quality < 50) {
                 quality = quality + 1;
 
-                if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (isBackstage()) {
                     if (sellIn < 11) {
                         if (quality < 50) {
                             quality = quality + 1;
@@ -46,27 +56,45 @@ public class Item {
                 }
             }
         }
+    }
 
-        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-            sellIn = sellIn - 1;
-        }
-
-        if (sellIn < 0) {
-            if (!name.equals("Aged Brie")) {
-                if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (quality > 0) {
-                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                            quality = quality - 1;
-                        }
+    public void updateQualityWhenExpiration() {
+        if (!isAgedBrie()) {
+            if (!isBackstage()) {
+                if (quality > 0) {
+                    if (!isSulfuras()) {
+                        quality = quality - 1;
                     }
-                } else {
-                    quality = 0;
                 }
             } else {
-                if (quality < 50) {
-                    quality = quality + 1;
-                }
+                quality = 0;
+            }
+        } else {
+            if (quality < 50) {
+                quality = quality + 1;
             }
         }
+    }
+
+    public boolean isExpired() {
+        return sellIn < 0;
+    }
+
+    public void updateSellIn() {
+        if (!isSulfuras()) {
+            sellIn = sellIn - 1;
+        }
+    }
+
+    public boolean isSulfuras() {
+        return name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    public boolean isBackstage() {
+        return name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    public boolean isAgedBrie() {
+        return name.equals("Aged Brie");
     }
 }
